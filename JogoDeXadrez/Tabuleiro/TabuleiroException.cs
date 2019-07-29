014380ebf441de76c5cfe929726f466b3e0d24cc
Matheus Paiva <matheus1298@gmail.com>
2019-07-29 11:09:54 -0300

    tratando erros com excecao personalizada

---
+++ JogoDeXadrez/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+
+        public TabuleiroException(String msg) : base(msg)
+        {
+
+        }
+
+    }
+}
