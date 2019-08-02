ebe7e8688b144f3b7fb9e6f7810e2eca45d9e4e8
Matheus Paiva <matheus1298@gmail.com>
2019-08-02 10:01:40 -0300

    jogada especial promocao

--- JogoDeXadrez/xadrez/PartidaDeXadrez.cs
+++ JogoDeXadrez/xadrez/PartidaDeXadrez.cs
@@ -177,6 +177,19 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+            Peca p = tab.peca(destino);
+            
+            //#jogadaespecial promocao
+
+            if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+            {
+                p = tab.retirarPeca(destino);
+                pecas.Remove(p);
+                Peca dama = new Dama(tab, p.Cor);
+                tab.colocarPeca(dama, destino);
+                pecas.Add(dama); 
+            }
+
 
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
@@ -199,7 +212,7 @@ namespace xadrez
 
             }
 
-            Peca p = tab.peca(destino);
+            
 
             //#jogadaespecial en Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
